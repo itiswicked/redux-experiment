3873b57f0b149a8e7180a542fd10de27852fb7b6
Nathan <nathanrbourke@gmail.com>
2016-08-05 22:33:29 -0400

    romve create store, add example reducer and tests

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createStore } from 'redux';
 import expect from 'expect';
 
-function counter(state, action) {
-  return state;
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state -1;
+    default:
+      return state;
+  }
 }
-
 
 expect(
   counter(0, {type: 'INCREMENT'})
@@ -16,4 +21,18 @@ expect(
 
 expect(
   counter(2, {type: 'DECREMENT'})
+).toEqual(1);
+
+expect(
+  counter(1, {type: 'DECREMENT'})
+).toEqual(0);
+
+expect(
+  counter(2, {type: 'SOMETHING_ELSE'})
 ).toEqual(2);
+
+expect(
+  counter(undefined, {})
+).toEqual(0);
+
+console.log("Tests Passed!");

b97df6e0aa2e3d37c1135f091c998de6866c2dce
Nathan <nathanrbourke@gmail.com>
2016-08-06 08:56:31 -0400

    create store from scratch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -11,6 +11,29 @@ const counter = (state = 0, action) => {
       return state;
   }
 }
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listers.filter(l => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
 
 const store = createStore(counter);
 

9c1855cb67fd8acbb306660548938216397ca398
Nathan <nathanrbourke@gmail.com>
2016-08-06 08:41:37 -0400

    add redux counter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import expect from 'expect';
+import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -11,28 +12,48 @@ const counter = (state = 0, action) => {
   }
 }
 
-expect(
-  counter(0, {type: 'INCREMENT'})
-).toEqual(1);
+const store = createStore(counter);
 
-expect(
-  counter(1, {type: 'INCREMENT'})
-).toEqual(2);
+const render = () => {
+  document.body.innerText = " " + store.getState();
+};
 
-expect(
-  counter(2, {type: 'DECREMENT'})
-).toEqual(1);
+store.subscribe(render);
+render();
 
-expect(
-  counter(1, {type: 'DECREMENT'})
-).toEqual(0);
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
 
-expect(
-  counter(2, {type: 'SOMETHING_ELSE'})
-).toEqual(2);
+// console.log(store.getState());
+//
+// store.dispatch({ type: 'INCREMENT' });
+// console.log(store.getState());
 
-expect(
-  counter(undefined, {})
-).toEqual(0);
+// ###
 
-console.log("Tests Passed!");
+// expect(
+//   counter(0, {type: 'INCREMENT'})
+// ).toEqual(1);
+//
+// expect(
+//   counter(1, {type: 'INCREMENT'})
+// ).toEqual(2);
+//
+// expect(
+//   counter(2, {type: 'DECREMENT'})
+// ).toEqual(1);
+//
+// expect(
+//   counter(1, {type: 'DECREMENT'})
+// ).toEqual(0);
+//
+// expect(
+//   counter(2, {type: 'SOMETHING_ELSE'})
+// ).toEqual(2);
+//
+// expect(
+//   counter(undefined, {})
+// ).toEqual(0);
+
+// console.log("Tests Passed!");

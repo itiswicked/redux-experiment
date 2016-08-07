b9249501882e196c9bf6b0e3ebfb3f800659b1dc
Nathan <nathanrbourke@gmail.com>
2016-08-07 12:56:56 -0400

    toggle todo completed reducer

--- src/main.js
+++ src/main.js
@@ -4,6 +4,33 @@ import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const toggleTodo = (todo) => {
+  return Object.assign({}, todo, {
+    completed: !todo.completed
+  })
+}
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+  deepFreeze(todoBefore)
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter)
+}
+
+testToggleTodo();
+console.log("Tests passed!");
+
 /////////////////////////////////
 
 // const addCounter = (list) => {

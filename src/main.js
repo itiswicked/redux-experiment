3031e1d45ba21b99726e92bdbcdc68494f5752bc
Nathan Bourke <nathanrbourke@gmail.com>
2016-08-08 10:37:07 -0400

    refactor todos reduce to call single todo reducer

--- src/main.js
+++ src/main.js
@@ -4,20 +4,31 @@ import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const todos = (state = [], action) => {
+const todo = (state, action) => {
   switch(action.type) {
     case 'ADD_TODO':
-      let newTodo = {
+      return {
         id: action.id,
         text: action.text,
         completed: false
       }
-      return state.concat([newTodo]);
     case 'TOGGLE_COMPLETED':
-      return state.map(todo => {
-        if(todo.id !== action.id) return todo;
-        return Object.assign({}, todo, {completed: !todo.completed});
-      })
+      if(state.id !== action.id) return state;
+      return Object.assign({}, state, {completed: !state.completed});
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_COMPLETED':
+      return state.map(t => todo(t, action))
     default:
       return state;
   }
